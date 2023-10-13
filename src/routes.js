84663a0df0f9e7f7c6856eb4128210427da6321b
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-13 10:27:09 -0700

    Upload all files

--- src/routes.js
+++ src/routes.js
@@ -11,17 +11,17 @@ const routes = [
     errorElement: <ErrorPage />
   },
   {
-    path: "/reactMovieTest/directors",
+    path: "/directors",
     element: <Directors />,
     errorElement: <ErrorPage />
   },
   {
-    path: "/reactMovieTest/actors",
+    path: "/actors",
     element: <Actors />,
     errorElement: <ErrorPage />
   },
   {
-    path: "/reactMovieTest/movie/:title",
+    path: "/movie/:title",
     element: <Movie />,
     errorElement: <ErrorPage />
   }
